fb4f9ba1fb88fee914eb258ad058378a2e47697f
Anastasia Schultis <anastasiaschultis@Anastasias-MBP.fios-router.home>
2017-10-05 18:09:20 -0400

    fixed indentation for html
    Also, took out clear lights and added #bulb to js file. As well as fixed spelling

--- js/app.js
+++ js/app.js
@@ -1,34 +1,31 @@
 $(function() {
   // 1. listen for a click event
 
-  $("#stopButton").click(redLightOn);
-  $("#slowButton").click(yellowLightOn);
-  $("#goButton").click(grennLightOn);
+  $("#stopButton").click(redLight);
+  $("#slowButton").click(yellowLight);
+  $("#goButton").click(greenLight);
 
 
-  function redLightOn() {
-    console.log("stop light clicked on");
-    clearLights()
-    $("#stopLight").css("backgroundColor", "red");
+  function redLight() {
+    $("#stopLight").css("background-color", "red");
+    $("#bulb").css("backgroundColor", "black");
   }
 
 
-  function yellowLightOn() {
-    console.log('slow light clicked on');
-    clearLights();
-    $("#slowButton").css("backgroundColor", "yellow");
+  function yellowLight() {
+    $("#slowLight").css("background-color", "yellow");
+    $("#bulb").css("backgroundColor", "black");
   }  
 
-  function greenLightOn() {
-    console.log('go light clicked on');
-    clearLights();
-    $("#goButton").css("backgroundColor", "green");
+  function greenLight() {
+    $("#goLight").css("background-color", "green");
+    $("#bulb").css("backgroundColor", "black");
   }
 
   function clearLights() {
-    $("#stopLight").css("backgroundColor" , "black");
-    $("#slowLight").css("backgroundColor" , "black");
-    $("#goLight").css("backgroundColor" , "balck");
+    $("#stopLight").css("background-color" , "black");
+    $("#slowLight").css("background-color" , "black");
+    $("#goLight").css("background-color" , "balck");
   }
 
 
