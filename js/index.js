5ffdcb1aacdbfced57e84065b8509c05ab5af009
kelseycoughlin <khcoughl@gmail.com>
2017-10-12 17:03:45 -0400

    HW#3 Revisions

--- js/index.js
+++ js/index.js
@@ -1,22 +1,55 @@
 $(function(){
 
-$('#stopButton').click(switchRed);
-
-function switchRed(){
-	$('#stopLight').css('background-color','red');
-}
-
-$('#slowButton').click(switchYellow);
-
-function switchYellow(){
-	$('#slowLight').css('background-color','yellow')
-}
-
-$('#goButton').click(switchGreen);
-
-function switchGreen(){
-	$('#goLight').css('background-color','green')
-}
+    $('#stopButton').click(switchRed);
+    $('#slowButton').click(switchYellow);
+    $('#goButton').click(switchGreen);
 
 
+    function ClearLights() {
+       $('.bulb').css('background-color', 'black'); 
+     }
+
+    function switchRed(){
+        ClearLights();
+        $('#stopLight').css('background-color','red');
+     }
+
+     function switchYellow(){
+       ClearLights();
+       $('#slowLight').css('background-color','yellow')
+     }
+
+     function switchGreen(){
+       ClearLights();
+       $('#goLight').css('background-color','green')
+     }
+  
 });
+
+// function ClearLights() {
+//   $('#traffic-light').css('background-color', 'black');
+// }
+
+// $('#stopButton').click(switchRed);
+
+// function switchRed(){
+//   ClearLights()
+// 	$('#stopLight').css('background-color','red');
+// }
+
+// $('#slowButton').click(switchYellow);
+
+// function switchYellow(){
+//   ClearLights()
+// 	$('#slowLight').css('background-color','yellow')
+// }
+
+// $('#goButton').click(switchGreen);
+
+// function switchGreen(){
+//   ClearLights()
+// 	$('#goLight').css('background-color','green')
+// }
+
+
+// });
